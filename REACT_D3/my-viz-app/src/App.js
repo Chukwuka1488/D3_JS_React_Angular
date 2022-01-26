f026a6ef0f4cc5993694e1adf2947e86065882f8
Chuks Akibot <juniorakibor@yahoo.com>
2022-01-26 17:56:25 +0100

    smiley created in react app

--- REACT_D3/my-viz-app/src/App.js
+++ REACT_D3/my-viz-app/src/App.js
@@ -1,16 +1,11 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Smiley Face Part I</title>
-    <style>
-      body {
-        margin: 0;
-        overflow: hidden;
-      }
-    </style>
-  </head>
-  <body>
-    <svg width="960" height="500">
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <svg width="960" height="500">
       <circle
         cx="480"
         cy="250"
@@ -22,5 +17,9 @@
       <circle cx="350" cy="180" fill="black" r="50"></circle>
       <circle cx="600" cy="180" fill="black" r="50"></circle>
     </svg>
-  </body>
-</html>
+      </header>
+    </div>
+  );
+}
+
+export default App;
